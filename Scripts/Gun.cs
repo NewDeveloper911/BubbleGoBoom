fd69415f3a30eb1eb1734f636eb4e382cd84b6d1
nana <nana.amfobrobbey@gmail.com>
2025-01-24 22:12:23 +0000

    Starting work on gun

---
+++ Scripts/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Gun variables")]
+    [SerializeField] float fireRate;
+    [SerializeField] float bulletSpeed;
+    //could do bulletSpeed here too
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
