a7110d945a16ece7ed515630dacec31ca4f7b0ee
nana <nana.amfobrobbey@gmail.com>
2025-01-24 22:57:52 +0000

    Starting the gun

--- Scripts/Gun.cs
+++ Scripts/Gun.cs
@@ -8,6 +8,13 @@ public class Gun : MonoBehaviour
     [SerializeField] float fireRate;
     [SerializeField] float bulletSpeed;
     //could do bulletSpeed here too
+    [Range(5,50)]
+    [SerializeField] int bulletRate;
+    [SerializeField] float bulletTimer; 
+
+    [Header("Aiming, i guess")]
+    [SerializeField] Gameobject bulletPrefab;
+    [SerializeField] Transform playerTransform;
 
     void Start()
     {
@@ -17,6 +24,12 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(bulletTimer > 0) bulletTimer -= Time.deltaTime;
+        //Checking if the user has attempted to fire
+        if(Input.GetButtonDown("Fire1")){
+            bulletTimer = 1.0f / bulletRate; //resetting the firing timer
+            //Creating a bullet
+            Instantiate(bulletPrefab, playerTransform, Quaternion.identity);
+        }
     }
 }
