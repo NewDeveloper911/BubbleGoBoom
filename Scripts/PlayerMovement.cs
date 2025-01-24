ff890cbd0aef3810e8af5f90f62dff3b78bc52d7
nana <nana.amfobrobbey@gmail.com>
2025-01-24 21:51:44 +0000

    Implementing player movement

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player movement")]
-    float playerSpeed;
+    [Range(1.0f,50.0f)]
+    [SerializeField] float playerSpeed;
+    [SerializeField] float userRight, userForward;
+
+    [Header("Camera settings")]
+    [SerializeField] Camera main_Camera;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Get player movement
+        userRight = Input.GetAxis("Horizontal");
+        userForward = Input.GetAxis("Vertical");
+        //Moving the player
+        transform.position += new Vector3(userRight*playerSpeed*Time.deltaTime, userForward*playerSpeed*Time.deltaTime,0);
+    }
+
+    //Should handle collisions with enemy
+    void onCollisionEnter(){
+
     }
 }
