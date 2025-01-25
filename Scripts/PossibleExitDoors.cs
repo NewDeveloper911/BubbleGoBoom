3b90593b22e8d68ce456df57e0dc96160f460161
Uzomefuna <vu24995@essex.ac.uk>
2025-01-25 23:08:38 +0000

    made room prefabs

--- Scripts/PossibleExitDoors.cs
+++ Scripts/PossibleExitDoors.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PossibleExitDoors : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform room;
+    
+    GameObject[] taggedChildArray;
+    // Call this method with the parent GameObject and the tag you're looking for
+    /* public List<Transform> GetTaggedChildrenWithTag(Transform parent, string tag)
     {
-        
+        List<Transform> taggedChildren = new List<Transform>();
+        GetTaggedChildrenRecursive(parent, tag, taggedChildren);
+        return taggedChildren;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetTaggedChildrenRecursive(Transform parent, string tag, List<Transform> taggedChildren)
     {
-        
+        foreach (Transform child in parent)
+        {
+            if (child.CompareTag(tag))  // Check if the child has the specified tag
+            {
+                taggedChildren.Add(child);
+            }
+
+            // Recursively check the child objects
+            GetTaggedChildrenRecursive(child, tag, taggedChildren);
+        }
+    } */
+
+    // Example usage
+    void Start()
+    {
+        // Specify the tag you're interested in
+        string tagToFind = "Finish";  // Replace with your tag
+        taggedChildArray = GameObject.FindGameObjectsWithTag(tagToFind);
+
+        // Print out all tagged children
+        foreach (GameObject child in taggedChildArray)
+        {
+            Debug.Log(child.name);
+        }
     }
 }
