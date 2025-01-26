2e6fee8f765df631145bd8085384481972b28548
nana <nana.amfobrobbey@gmail.com>
2025-01-26 00:37:52 +0000

    Boss update

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -23,7 +23,7 @@ public class Enemy : MonoBehaviour
     [Range(1,10)]
     [SerializeField] int directionsFired;
     [Range(1,20f)] [SerializeField] float shootforce;
-    [Range(25, 200)]
+    [Range(25, 1000)]
     public int maxhealth;
     [SerializeField] int currentHealth;
 
@@ -61,39 +61,38 @@ public class Enemy : MonoBehaviour
             //Checking if we are close enough to start attacking them
             if(Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), attackRange, playerLayerMask)){
                 //Start coding the attacking and damage towards the player
-                if(isRanged){
+                if(isRanged && attackTimer <= 0){
                     //Implementing the cooldown
-                    if(attackTimer <= 0){
-                        if(directionsFired == 1){
-                            //Should shoot a projectile designed to hurt the player, otherwise, the player takes damage
-                            Vector3 bulletDirection = player.transform.position - transform.position;
-                            //Instantiate the bullet and force of it
+
+                    if(directionsFired == 1){
+                        //Should shoot a projectile designed to hurt the player, otherwise, the player takes damage
+                        Vector3 bulletDirection = player.transform.position - transform.position;
+                        //Instantiate the bullet and force of it
+                        GameObject curBullet = Instantiate(projectile, transform.position, Quaternion.identity);
+        
+                        //Shoot the bullet
+                        Bullet bullet = curBullet.GetComponent<Bullet>();
+                        bullet.targetEnemyMask = playerLayerMask;
+                        bullet.bulletDamage = damage;
+                        curBullet.GetComponent<Rigidbody2D>().AddForce(bulletDirection.normalized * shootforce, ForceMode2D.Impulse);
+
+                    }
+                    else{
+                        //This should handle attacks of multiple bullets in directions at once
+                        for(int i=1;i<directionsFired;i++){
+                            float angle = 360f / directionsFired * i;
+                            Vector3 direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0);;
+                                //Instantiate the bullet and force of it
                             GameObject curBullet = Instantiate(projectile, transform.position, Quaternion.identity);
             
                             //Shoot the bullet
                             Bullet bullet = curBullet.GetComponent<Bullet>();
                             bullet.targetEnemyMask = playerLayerMask;
                             bullet.bulletDamage = damage;
-                            curBullet.GetComponent<Rigidbody2D>().AddForce(bulletDirection.normalized * shootforce, ForceMode2D.Impulse);
-   
+                            curBullet.GetComponent<Rigidbody2D>().AddForce(direction * shootforce, ForceMode2D.Impulse);
                         }
-                        else{
-                            //This should handle attacks of multiple bullets in directions at once
-                            for(int i=1;i<directionsFired;i++){
-                                float angle = 360f / directionsFired * i;
-                                Vector3 direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0);;
-                                 //Instantiate the bullet and force of it
-                                GameObject curBullet = Instantiate(projectile, transform.position, Quaternion.identity);
-                
-                                //Shoot the bullet
-                                Bullet bullet = curBullet.GetComponent<Bullet>();
-                                bullet.targetEnemyMask = playerLayerMask;
-                                bullet.bulletDamage = damage;
-                                curBullet.GetComponent<Rigidbody2D>().AddForce(direction * shootforce, ForceMode2D.Impulse);
-                            }
-                        }
-                        attackTimer = attackCooldown;
-                    } 
+                    }
+                    attackTimer = attackCooldown;  
 
                 }
             }
